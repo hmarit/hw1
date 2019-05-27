ae99bdfcb6c43a2261bcd244cc9022e46ea3dc18
hmarit <hmaritsunoda@gmail.com>
2019-05-27 18:26:45 +0900

    Added functions naiveReorder and createDictionary.cpp file.

--- README.md
+++
@@ -1,2 +0,0 @@
-# hw1
-STEP Homework #1
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include "func.h"
+using namespace std;
+
+int main()
+{
+  string input, upperInput;
+
+  // Create alphabetically reorganized dictionary
+  // ie. butter -> berttu
+  alphabeticallyReorderedDict();
+  set<string> dictionary;
+  cin >>
+
+  /*
+  cin >> input;
+  upperInput = toUpper(input);
+  sortedInput = sortAlphabetically(upperInput);
+  */
+}

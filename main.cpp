aef7133774ba172ce030af034f7c3dcb5b5043c0
hmarit <hmaritsunoda@gmail.com>
2019-05-28 17:55:20 +0900

    Sorted dictionary row-wise

--- main.cpp
+++ main.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <fstream>
 #include "func.h"
 using namespace std;
 
 int main()
 {
-  string input, upperInput;
-
-  // Create alphabetically reorganized dictionary
-  // ie. butter -> berttu
-  alphabeticallyReorderedDict();
+  string word;
   set<string> dictionary;
-  cin >>
+
+  fstream file ("reorderedDictionary.txt"); // reorderedDictionary.txt
+  if (!file.is_open())
+    cout << "File cannot be opened!" << endl;
+  else
+  {
+    while (!file.eof())
+    {
+      file >> word;
+      dictionary.insert(word);
+    }
+  }
+
+  // Check
+  for (set<string>::iterator it = dictionary.begin(); it != dictionary.end(); it++)
+  {
+    cout << *it << ' ';
+  }
+
 
   /*
   cin >> input;
